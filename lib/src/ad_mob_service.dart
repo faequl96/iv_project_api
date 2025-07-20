64c45fa417ae48f0a221d660d8af73e6dd9513c1
faiq <faiq@voltras.co.id>
2025-07-20 22:48:23 +0700

    save...

---
+++ lib/src/ad_mob_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:iv_project_api_core/iv_project_api_core.dart';
+import 'package:iv_project_model/iv_project_model.dart';
+
+class AdMobService {
+  final Dio _dio = ApiClient.dio;
+
+  Future<IVCoinResponse> addExtraIVCoins(AdMobRequest request) async {
+    try {
+      final response = await _dio.post(AdMobEndpoints.addExtraIVCoins, data: request.toJson());
+      return IVCoinResponse.fromJson(response.data['data']);
+    } on DioException catch (error) {
+      throw ApiException.fromDioError(error);
+    }
+  }
+}
---
+++ lib/src/iv_coin_service.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:iv_project_api_core/iv_project_api_core.dart';
+import 'package:iv_project_model/iv_project_model.dart';
+
+class IVCoinService {
+  final Dio _dio = ApiClient.dio;
+
+  Future<IVCoinResponse> get() async {
+    try {
+      final response = await _dio.get(IVCoinEndpoints.get);
+      return IVCoinResponse.fromJson(response.data['data']);
+    } on DioException catch (error) {
+      throw ApiException.fromDioError(error);
+    }
+  }
+
+  Future<IVCoinResponse> getById(int id) async {
+    try {
+      final response = await _dio.get('${IVCoinEndpoints.getById}$id');
+      return IVCoinResponse.fromJson(response.data['data']);
+    } on DioException catch (error) {
+      throw ApiException.fromDioError(error);
+    }
+  }
+
+  Future<IVCoinResponse> updateById(int id, IVCoinRequest request) async {
+    try {
+      final response = await _dio.patch('${IVCoinEndpoints.updateById}$id', data: request.toJson());
+      return IVCoinResponse.fromJson(response.data['data']);
+    } on DioException catch (error) {
+      throw ApiException.fromDioError(error);
+    }
+  }
+}
