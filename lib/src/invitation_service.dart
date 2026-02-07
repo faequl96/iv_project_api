38a80e0581e9d6a906f2112488f1eea71582f6dc
faiq <faiq@voltras.co.id>
2026-02-07 17:35:37 +0700

    up

--- lib/src/invitation_service.dart
+++ lib/src/invitation_service.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:iv_project_api_core/iv_project_api_core.dart';
 import 'package:iv_project_model/iv_project_model.dart';
+import 'package:path/path.dart' as p;
 
 class InvitationService {
   final Dio _dio = ApiClient.dio;
@@ -11,12 +12,21 @@ class InvitationService {
     try {
       final formData = FormData.fromMap({
         'invitation': jsonEncode(request.toJson()),
-        'bride_nickname': request.invitationData.bride.nickname,
-        'groom_nickname': request.invitationData.groom.nickname,
+        'bride_nickname': request.invitationData.bride.nickname.split(' ').first,
+        'groom_nickname': request.invitationData.groom.nickname.split(' ').first,
+        'event_time': DateTimeConverter.toJson(
+          request.invitationData.receptionEvent.startTime,
+        )!.substring(0, 16).replaceAll('-', '_').replaceAll(' ', '_').replaceAll(':', '_'),
         if (imageRequest != null)
-          ...(await imageRequest.toFormDataMap((coverImage, brideImage, groomImage, galleries) async {
+          ...(await imageRequest.toFormDataMap((musicAudio, coverImage, brideImage, groomImage, galleries) async {
             final map = <String, dynamic>{};
 
+            if (musicAudio != null) {
+              map['music_audio'] = await MultipartFile.fromFile(
+                musicAudio.path,
+                filename: p.basename(musicAudio.path).replaceAll(' ', '_'),
+              );
+            }
             if (coverImage != null) map['cover_image'] = await MultipartFile.fromFile(coverImage.path, filename: 'cover_image');
             if (brideImage != null) map['bride_image'] = await MultipartFile.fromFile(brideImage.path, filename: 'bride_image');
             if (groomImage != null) map['groom_image'] = await MultipartFile.fromFile(groomImage.path, filename: 'groom_image');
@@ -58,23 +68,27 @@ class InvitationService {
     }
   }
 
-  Future<List<InvitationResponse>> getsByUserId(String userId) async {
-    try {
-      final response = await _dio.get('${InvitationEndpoints.getsByUserId}$userId');
-      return (response.data['data'] as List).map((json) => InvitationResponse.fromJson(json)).toList();
-    } on DioException catch (error) {
-      throw ApiException.fromDioError(error);
-    }
-  }
-
   Future<InvitationResponse> updateById(String id, UpdateInvitationRequest request, InvitationImageRequest? imageRequest) async {
     try {
       final formData = FormData.fromMap({
         'invitation': jsonEncode(request.toJson()),
+        'bride_nickname': request.invitationData?.bride?.nickname?.split(' ').first,
+        'groom_nickname': request.invitationData?.groom?.nickname?.split(' ').first,
+        'event_time': request.invitationData?.receptionEvent?.startTime != null
+            ? DateTimeConverter.toJson(
+                request.invitationData?.receptionEvent?.startTime,
+              )!.substring(0, 16).replaceAll('-', '_').replaceAll(' ', '_').replaceAll(':', '_')
+            : null,
         if (imageRequest != null)
-          ...(await imageRequest.toFormDataMap((coverImage, brideImage, groomImage, galleries) async {
+          ...(await imageRequest.toFormDataMap((musicAudio, coverImage, brideImage, groomImage, galleries) async {
             final map = <String, dynamic>{};
 
+            if (musicAudio != null) {
+              map['music_audio'] = await MultipartFile.fromFile(
+                musicAudio.path,
+                filename: p.basename(musicAudio.path).replaceAll(' ', '_'),
+              );
+            }
             if (coverImage != null) map['cover_image'] = await MultipartFile.fromFile(coverImage.path, filename: 'cover_image');
             if (brideImage != null) map['bride_image'] = await MultipartFile.fromFile(brideImage.path, filename: 'bride_image');
             if (groomImage != null) map['groom_image'] = await MultipartFile.fromFile(groomImage.path, filename: 'groom_image');
